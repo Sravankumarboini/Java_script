3364b367e1ce56bcc1e70391d59a875c73795582
Sravankumarboini <sravankumarboini005@gmail.com>
2026-01-08 16:50:46 +0530

    using promise with then and catch

--- advance/app.js
+++ advance/app.js
@@ -2,8 +2,25 @@
 //     console.log("i am a promise");
 //     resolve("success");
 // });
+//-----------------------------------------------------------------
 
+const getPromise = () => {
+    return new Promise((resolve, reject)=>{
+        console.log("i am a promise");
+        resolve("success");
+    });
+};
 
+let promise = getPromise();
+promise.then((res)=>{
+    console.log("promise fulfilled", res);
+});
+
+promise.catch((err)=>{
+    console.log("promise rejected");
+})
+
+//-----------------------------------------------------------------
 // console.log("one");
 
 // function hello(){
@@ -15,18 +32,18 @@
 
 // console.log("two");
 
-
-function getData(dataId, getNextData){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            console.log("data", dataId);
-            resolve("success");
-            if(getNextData){
-                getNextData();
-            }
-        }, 5000);
-    })
-}
+//-----------------------------------------------------------------
+// function getData(dataId, getNextData){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             console.log("data", dataId);
+//             resolve("success");
+//             if(getNextData){
+//                 getNextData();
+//             }
+//         }, 5000);
+//     })
+// }
 // //callback hell
 // getData(1, ()=>{
 //     getData(2, ()=>{
