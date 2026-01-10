7225456206b8c2c781c3694b449c7f13525d91b2
Sravankumarboini <sravankumarboini005@gmail.com>
2026-01-10 20:37:07 +0530

    getting data into the html page

--- advance/app1.js
+++ advance/app1.js
@@ -1,9 +1,12 @@
 const URL = "https://cat-fact.herokuapp.com/facts";
+const factPara = document.querySelector("#fact");
+const btn = document.querySelector("#btn");
 
 const getFacts = async () =>{
     let response = await fetch(URL);
-    console.log(response);
+    console.log(response); //json format
     let data = await response.json();
-    console.log(data);
+    factPara.innerHTML = data[0].text;
 };
 
+btn.addEventListener("click", getFacts);
