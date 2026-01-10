636d3f1aa4d7fc30bcfd6d43dddfb44c02bbf29b
Sravankumarboini <sravankumarboini005@gmail.com>
2026-01-10 07:45:00 +0530

    async await standard ex

--- advance/app.js
+++ advance/app.js
@@ -89,16 +89,36 @@
 
 // hello();
 
-function api(){
+// function api(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             console.log("weather data..");
+//             resolve(200);
+//         }, 2000);
+//     });
+// };
+
+// async function getWeatherData(){
+//     await api();  //1st
+//     await api(); //2nd 
+// }
+
+//Aync Await
+
+function getData(dataId){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            console.log("weather data..");
-            resolve(200);
+            console.log("data", dataId);
+            resolve("success");
         }, 2000);
     });
-};
+}
 
-async function getWeatherData(){
-    await api();  //1st
-    await api(); //2nd 
+async function getAllData(){
+    console.log("getting data1...");
+    await getData(1);
+    console.log("getting data2...");
+    await getData(2);
+    console.log("getting data3...");
+    await getData(3);
 }
