3976cafeb9b5405b0b5214174bae93efddbd9a3d
Sravankumarboini <sravankumarboini005@gmail.com>
2026-01-13 06:17:39 +0530

    import country list from code.js to app.js

--- advance/CurrencyConverter/app.js
+++ advance/CurrencyConverter/app.js
@@ -1,2 +1,13 @@
+import { countryList } from "./code.js";
+
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/inr.json";
 
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for(let code in countryList){
+    console.log(code);
+}
+
+for(let select of dropdowns){
+
+}
